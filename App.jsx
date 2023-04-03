3e55906f43502e0de09ca1df2346c177b42cf060
Kristen Garnier <kristen@oktopod.io>
2023-04-03 15:26:11 +0200

    fix : return error

--- App.jsx
+++ App.jsx
@@ -39,7 +39,7 @@ function App() {
 	useEffect(() => {
 		async function checkToken() {
 			const token = await SecureStore.getItemAsync(TOKEN_KEY);
-			if (!token) setConnected(false);
+			if (!token) return setConnected(false);
 
 			return setConnected(true);
 		}
